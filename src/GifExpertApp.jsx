1daadbc8f030edb311e6089c025047de4a8741a5
ChristianF10 <seriesx.christian@outlook.com>
2023-03-09 17:17:04 -0300

    update GifExpertApp

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,24 +1,37 @@
-import { useState } from "react";
-import { AddCategory } from "./components/AddCategory";
-import { GifGrid } from "./components/GifGrid";
+import { useState } from 'react';
+import { AddCategory, GifGrid } from './components';
 
 export const GifExpertApp = () => {
-  const [categories, setCategories] = useState(["Dragon Ball"]);
+    
+    const [ categories, setCategories ] = useState([ 'One Punch' ]);
+    
+    const onAddCategory = ( newCategory ) => {
+        if ( categories.includes(newCategory) ) return;
+        setCategories([ newCategory, ...categories ]);
+    }
+    
 
-  const onAddCategory = (newCategory) => {
-    if (categories.includes(newCategory)) return;
-    setCategories([newCategory, ...categories]);
-  };
+    return (
+        <>
 
-  return (
-    <>
-      <h1>GifExpertApp</h1>
+            <h1>GifExpertApp</h1>
 
-      <AddCategory onNewCategory={onAddCategory} />
+    
+            <AddCategory 
+                onNewCategory={ (value) => onAddCategory(value) }
+            />
 
-        {categories.map((category) => (
-          <GifGrid  key={category} category={category}/>
-        ))}
-    </>
-  );
-};
+            { 
+                categories.map( ( category ) => (
+                    <GifGrid 
+                        key={ category } 
+                        category={ category } />
+                ))
+            }
+
+
+
+
+        </>
+    )
+}
